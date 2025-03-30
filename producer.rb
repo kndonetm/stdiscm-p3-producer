6810ce5b3e27d36e7d41c5eabd68e0e5949f4996
kndone <justinlim4916@gmail.com>
2025-03-31 02:51:56 +0800

    Initial Commit

---
+++ producer.rb
@@ -0,0 +1,35 @@
+require 'dotenv/load'
+require "socket"
+require 'json'
+
+def send_command(msg, socket)
+    puts msg.length
+    socket.write [msg.length].pack("q")
+    socket.write msg
+end
+
+TCPSocket.open(ENV["HOSTNAME"], ENV["PORT"]) do |socket| 
+    videos = Dir.children(ENV["VIDEO_DIRECTORY"]).select { |filename|
+        filename.end_with?(".mp4") or filename.end_with?(".mkv") or filename.end_with?(".mov")
+    }
+    .map { |filename|
+        File.join(ENV["VIDEO_DIRECTORY"], filename)
+    }
+
+    videos.each do |video|
+        name = File.basename(video)
+        data = IO.binread(video)
+        size = data.length
+        
+        cmd = {
+            action: "sendFile",
+            size: size,
+            filename: name
+        }
+
+        send_command(JSON.generate(cmd), socket)
+        socket.write data
+    end    
+
+    socket.close
+end
