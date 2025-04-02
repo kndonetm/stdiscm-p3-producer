4972f29dc0f95f9054bf955826965237b6677739
kndone <justinlim4916@gmail.com>
2025-04-02 16:58:42 +0800

    feat: multiple input directories

--- producer.rb
+++ producer.rb
@@ -5,17 +5,22 @@ class VideoClient
     def initialize(settings)
         @hostname = settings["HOSTNAME"]
         @port = settings["PORT"]
-        @video_directory = settings["VIDEO_DIRECTORY"]
+
+        @video_dirs = settings["VIDEO_DIRECTORIES"].split(",")
     end
 
-    def send_video(video, socket)
-        puts "Attempting to send video to server"
+    def send_video(video, socket, filename=nil)
+        if filename == nil then
+            filename = "video"
+        end
+        puts "Attempting to send #{filename} to server"
+
         socket.write video
         wait_for_ok socket
     end
 
-    def send_command(msg, socket)
-        puts "Attempting to send command #{msg} to server"
+    def send_json(msg, socket)
+        puts "Attempting to send #{msg} to server"
         socket.write [msg.length].pack("q")
         socket.write msg
         wait_for_ok socket
@@ -31,36 +36,38 @@ class VideoClient
         end
     end
 
+    def get_videos_in_directory(dirname)
+        return Dir.children(dirname).select { |filename|
+            filename.end_with?(".mp4") or filename.end_with?(".mkv") or filename.end_with?(".mov")
+        }
+        .map { |filename|
+            File.join(dirname, filename)
+        }
+    end
+
     def upload
         puts "Attempting to connect to #{@hostname} Port #{@port}"
         @socket = TCPSocket.open(@hostname, @port)
 
-        videos = Dir.children(@video_directory).select { |filename|
-            filename.end_with?(".mp4") or filename.end_with?(".mkv") or filename.end_with?(".mov")
-        }
-        .map { |filename|
-            File.join(@video_directory, filename)
-        }
-    
-        videos.each do |video|
-            name = File.basename(video)
-            data = IO.binread(video)
-            size = data.length
-            
-            cmd = {
-                action: "sendFile",
-                size: size,
-                filename: name
-            }
-    
-            send_command JSON.generate(cmd), @socket
-            send_video data, @socket
-        end    
-    
+        @video_dirs.each do |dirname|
+            videos = get_videos_in_directory dirname
+
+            videos.each do |video|
+                name = File.basename(video)
+                data = IO.binread(video)
+                size = data.length
+                
+                cmd = {
+                    action: "sendFile",
+                    size: size,
+                    filename: name
+                }
+        
+                send_json JSON.generate(cmd), @socket
+                send_video data, @socket, File.join(video)
+            end   
+        end
+
         @socket.close
     end
 end
-
-
-
-
